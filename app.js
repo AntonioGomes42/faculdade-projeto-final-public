8108bc8b84e67fc677cec6e7efc6bc7e1078a458
AntonioGomes42 <br.com.antoniogomes@gmail.com>
2022-05-30 14:50:57 -0300

    Added advices page

--- app.js
+++ app.js
@@ -2,16 +2,35 @@ import express from "express";
 import path from "path";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import favicon from "serve-favicon";
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(favicon(path.join(__dirname, 'public', 'images/favicon.ico')))
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/public/templates'));
 
+function returnOnConstructionPage(res) {
+  res.render('on-construction');
+}
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/avisos', (req, res) => {
+  res.render('advices');
+});
+
+app.get('/eventos', (req, res) => {
+  returnOnConstructionPage(res)
+});
+
+app.get('/midias', (req, res) => {
+  returnOnConstructionPage(res)
 });
 
 app.listen(port, () => {
